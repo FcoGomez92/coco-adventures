37a5dd38ce1c86c0581ca4ac91e70ac98499279f
Francisco Manuel Gómez Gallardo <francis.gg48@gmail.com>
2022-06-10 18:11:44 +0200

    continue adding functions explanation

--- core/rarity_crafting-materials-1.sol
+++ core/rarity_crafting-materials-1.sol
@@ -27,17 +27,20 @@ interface attributes {
         );
 }
 
+// parece que simula ser un erc-20
 contract rarity_crafting_materials {
     string public constant name = "Rarity Crafting Materials (I)";
     string public constant symbol = "Craft (I)";
     uint8 public constant decimals = 18;
 
+    // configuracion niveles de una dungeon
     int256 public constant dungeon_health = 10;
     int256 public constant dungeon_damage = 2;
     int256 public constant dungeon_to_hit = 3;
     int256 public constant dungeon_armor_class = 2;
     uint256 constant DAY = 1 days;
 
+    // devuelve la salud segun clase
     function health_by_class(uint256 _class)
         public
         pure
@@ -68,6 +71,7 @@ contract rarity_crafting_materials {
         }
     }
 
+    // calculo de la salud añadiendole el nivel y constitucion
     function health_by_class_and_level(
         uint256 _class,
         uint256 _level,
@@ -81,6 +85,7 @@ contract rarity_crafting_materials {
         health = uint256(_base_health) * _level;
     }
 
+    // calculo de bonus de ataque segun clase
     function base_attack_bonus_by_class(uint256 _class)
         public
         pure
@@ -111,6 +116,7 @@ contract rarity_crafting_materials {
         }
     }
 
+    // calculo de bonus de ataque añadiendole el nivel
     function base_attack_bonus_by_class_and_level(
         uint256 _class,
         uint256 _level
@@ -118,6 +124,7 @@ contract rarity_crafting_materials {
         return (_level * base_attack_bonus_by_class(_class)) / 4;
     }
 
+    // modificador segun nivel de atributo.
     function modifier_for_attribute(uint256 _attribute)
         public
         pure
@@ -129,6 +136,7 @@ contract rarity_crafting_materials {
         return (int256(_attribute) - 10) / 2;
     }
 
+    // calculo de bonus de ataque. Se usa la funcion creada anteriormente
     function attack_bonus(
         uint256 _class,
         uint256 _str,
@@ -139,10 +147,12 @@ contract rarity_crafting_materials {
             modifier_for_attribute(_str);
     }
 
+    // devuelve true o false, e indica si el jugador puede atacar a la dungeon
     function to_hit_ac(int256 _attack_bonus) public pure returns (bool) {
         return (_attack_bonus > dungeon_armor_class);
     }
 
+    // Calculo del daño segun la fuerza del jugador
     function damage(uint256 _str) public pure returns (uint256) {
         int256 _mod = modifier_for_attribute(_str);
         if (_mod <= 1) {
@@ -152,33 +162,52 @@ contract rarity_crafting_materials {
         }
     }
 
+    // devuelve el poder de defensa del jugador
     function armor_class(uint256 _dex) public pure returns (int256) {
         return modifier_for_attribute(_dex);
     }
 
+    // funcion pelear con una dungeon
     function scout(uint256 _summoner) public view returns (uint256 reward) {
+        // nivel del personaje
         uint256 _level = rm.level(_summoner);
+        // clase del personaje
         uint256 _class = rm.class(_summoner);
+        //  fuerza, destreza y constitucion del personaje
         (uint32 _str, uint32 _dex, uint32 _const, , , ) = _attr.ability_scores(
             _summoner
         );
+        // salud del personaje
         int256 _health = int256(
             health_by_class_and_level(_class, _level, _const)
         );
+        // salud de la dungeon
         int256 _dungeon_health = dungeon_health;
+        // daño que hace el personaje
         int256 _damage = int256(damage(_str));
+        // bonus de ataque del personaje
         int256 _attack_bonus = attack_bonus(_class, _str, _level);
+        // indicador de si el jugador puede atacar
         bool _to_hit_ac = to_hit_ac(_attack_bonus);
+        // indicador de ataque del dungeon (si nuestra defensa es menor que el golpe de la dungeon)
         bool _hit_ac = armor_class(_dex) < dungeon_to_hit;
+
+        // si el jugador puede atacar...
         if (_to_hit_ac) {
+            // ...entramos en un bucle de 10 rondas en el que ambos atacan.
             for (reward = 10; reward >= 0; reward--) {
+                // le quitamos vida a la dungeon
                 _dungeon_health -= _damage;
+                // si le hemos vencido el bucle termina y devuelve un reward que depende
+                //  de los ciclos de ataque que hayamos hecho
                 if (_dungeon_health <= 0) {
                     break;
                 }
+                // si la dungeon nos puede atacar nos resta vida con su ataque
                 if (_hit_ac) {
                     _health -= dungeon_damage;
                 }
+                // si nos mata se para el bucle y recibimos 0
                 if (_health <= 0) {
                     return 0;
                 }
@@ -186,28 +215,39 @@ contract rarity_crafting_materials {
         }
     }
 
+    // funcion que inicia una aventura. Devuelve una recompensa
     function adventure(uint256 _summoner) external returns (uint256 reward) {
+        // comprueba que seas autorizado o dueño del personaje
         require(_isApprovedOrOwner(_summoner));
+        // comprueba que no hayas hecho otra en las ultimas 24h
         require(block.timestamp > adventurers_log[_summoner]);
+        // actualiza el log con la nueva fecha desde la que contabilizar la proxima adventure
         adventurers_log[_summoner] = block.timestamp + DAY;
+        // calcula tu recompensa despues de la pelea
         reward = scout(_summoner);
+        // mintea la recompensa y se la da al personaje
         _mint(_summoner, reward);
     }
 
+    // inicializa el supply del material
     uint256 public totalSupply = 0;
 
     rarity constant rm = rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
     attributes constant _attr =
         attributes(0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1);
 
+    // registro del allowance. relaciona personajes con cantidades aprovadas a terceros
     mapping(uint256 => mapping(uint256 => uint256)) public allowance;
+    // registro de balances
     mapping(uint256 => uint256) public balanceOf;
 
+    // registro de ultimo log del personaje
     mapping(uint256 => uint256) public adventurers_log;
 
     event Transfer(uint256 indexed from, uint256 indexed to, uint256 amount);
     event Approval(uint256 indexed from, uint256 indexed to, uint256 amount);
 
+    // funciones que simulan ser erc-20
     function _isApprovedOrOwner(uint256 _summoner)
         internal
         view
